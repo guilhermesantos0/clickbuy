50b40a7672cba668cf639b92b33d46fa62c67fd9
guilhermesantos0 <guilhermesantosdev@outlook.com>
2025-04-28 12:57:48 -0300

    feat: notify

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -4,13 +4,20 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/cadastro" element={<Cadastro />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+
+      <ToastContainer style={{ zIndex: 9999 }} theme="colored" />
+    </>
   );
 }
 
