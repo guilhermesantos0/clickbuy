def616feb055f653dae95d8898aabebded6bc428
guilhermesantos0 <guilhermesantosdev@outlook.com>
2025-05-05 12:48:28 -0300

    dev: editar perfil

--- site/src/pages/EditarPerfil/index.tsx
+++ site/src/pages/EditarPerfil/index.tsx
@@ -1,67 +1,100 @@
 import { useState } from 'react';
 import { useUser } from 'contexts/UserContext';
-import style from './EditarPerfil.module.scss';
+
+import style from './css/EditarPerfil.module.scss';
+import firstTab from './css/FirstTab.module.scss';
+import secondTab from './css/SecondTab.module.scss'
 
 import Header from '../../components/Header';
 
 const EditarPerfil = () => {
     const { user } = useUser();
-    const [tab, setTab] = useState<'login' | 'personal' | 'address'>('login');
+    const [tab, setTab] = useState(1);
 
     return (
         <div className={style.Container}>
-            <Header user={user} />
+            <Header hideOptions={true} user={user} />
+            <div className={style.PageContent}>
 
-            <div className={style.Tabs}>
-                <button className={tab === 'login' ? style.ActiveTab : ''} onClick={() => setTab('login')}>Dados de Login</button>
-                <button className={tab === 'personal' ? style.ActiveTab : ''} onClick={() => setTab('personal')}>Dados Pessoais</button>
-                <button className={tab === 'address' ? style.ActiveTab : ''} onClick={() => setTab('address')}>Endereço</button>
-            </div>
+                <div className={style.Tabs} style={{ "--active-tab-index": tab - 2 } as React.CSSProperties}>
+                    <div className={`${style.Tab} ${tab === 1 ? style.ActiveTab : ''}`} onClick={() => setTab(1)}>Dados de Login</div>
+                    <div className={`${style.Tab} ${tab === 2 ? style.ActiveTab : ''}`} onClick={() => setTab(2)}>Dados Pessoais</div>
+                    <div className={`${style.Tab} ${tab === 3 ? style.ActiveTab : ''}`} onClick={() => setTab(3)}>Endereço</div>
+                </div>
 
-            <div className={style.Content}>
-                {tab === 'login' && (
+                {tab === 1 && (
+                    <div className={firstTab.Container}>
+                        <div className={firstTab.InputGroup}>
+                            <label>Email</label>
+                            <input className={firstTab.Input} type="email" value={user?.email} />
+                        </div>
+                        <div className={firstTab.InputGroup}>
+                            <label>Senha</label>
+                            <input className={firstTab.Input} type="password" value={user?.password} placeholder="Nova senha" />
+                        </div>
+                    </div>
+                )}
+                {tab === 2 && (
+                    <div className={secondTab.Container}>
+                        <div className={style.InputGroup}>
+                            <label>Foto de Perfil</label>
+                            <input type="text" value={user?.profilePic} placeholder="URL da imagem" />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Nome</label>
+                            <input type="text" value={user?.personalData.name} />
+
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Data de Nascimento</label>
+                            <input type="date" value={user?.personalData.bornDate} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>CPF</label>
+                            <input type="text" value={user?.personalData.cpf} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Telefone</label>
+                            <input type="text" value={user?.personalData.phone} />
+                        </div>
+                    </div>
+                )}
+                {tab === 3 && (
                     <>
-                        <h2>Dados de Login</h2>
-                        <label>Email</label>
-                        <input type="email" defaultValue={user?.email} />
-                        <label>Senha</label>
-                        <input type="password" placeholder="Nova senha" />
+                        <div className={style.InputGroup}>
+                            <label>Rua</label>
+                            <input type="text" defaultValue={user?.personalData.address.road} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Número</label>
+                            <input type="text" defaultValue={user?.personalData.address.number} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Bairro</label>
+                            <input type="text" defaultValue={user?.personalData.address.neighborhood} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Complemento</label>
+                            <input type="text" defaultValue={user?.personalData.address.complement} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Cidade</label>
+                            <input type="text" defaultValue={user?.personalData.address.city} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>Estado</label>
+                            <input type="text" defaultValue={user?.personalData.address.state} />
+                        </div>
+                        <div className={style.InputGroup}>
+                            <label>CEP</label>
+                            <input type="text" defaultValue={user?.personalData.address.zip} />
+                        </div>
                     </>
                 )}
-                {tab === 'personal' && (
-                    <>
-                        <h2>Dados Pessoais</h2>
-                        <label>Foto de Perfil</label>
-                        <input type="text" defaultValue={user?.profilePic} placeholder="URL da imagem" />
-                        <label>Nome</label>
-                        <input type="text" defaultValue={user?.personalData.name} />
-                        <label>Data de Nascimento</label>
-                        <input type="date" defaultValue={user?.personalData.bornDate} />
-                        <label>CPF</label>
-                        <input type="text" defaultValue={user?.personalData.cpf} />
-                        <label>Telefone</label>
-                        <input type="text" defaultValue={user?.personalData.phone} />
-                    </>
-                )}
-                {tab === 'address' && (
-                    <>
-                        <h2>Endereço</h2>
-                        <label>Rua</label>
-                        <input type="text" defaultValue={user?.personalData.address.road} />
-                        <label>Número</label>
-                        <input type="text" defaultValue={user?.personalData.address.number} />
-                        <label>Bairro</label>
-                        <input type="text" defaultValue={user?.personalData.address.neighborhood} />
-                        <label>Complemento</label>
-                        <input type="text" defaultValue={user?.personalData.address.complement} />
-                        <label>Cidade</label>
-                        <input type="text" defaultValue={user?.personalData.address.city} />
-                        <label>Estado</label>
-                        <input type="text" defaultValue={user?.personalData.address.state} />
-                        <label>CEP</label>
-                        <input type="text" defaultValue={user?.personalData.address.zip} />
-                    </>
-                )}
+                <div className={style.ButtonsArea}>
+                    <button className={style.Save}>Salvar</button>
+                    <button className={style.Cancel}>Cancelar</button>
+                </div>
             </div>
         </div>
     );
