ef1ef1f08c7581ab56be80c0760729f0f536a46c
Guilherme Santos <guilhermesantosdev@outlook.com>
2025-05-06 00:13:37 -0300

    dev: anunciar produto

--- site/src/pages/Anunciar/index.tsx
+++ site/src/pages/Anunciar/index.tsx
@@ -4,7 +4,35 @@ import { Category } from "@modules/Category";
 
 import style from './Anunciar.module.scss';
 
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
+import { useUser } from "contexts/UserContext";
+
+interface State {
+    id: number,
+    sigla: string,
+    nome: string
+}
+
+interface City {
+    id: number,
+    nome: string
+}
+
 const Anunciar = () => {
+
+    const { user } = useUser();
+
+    const [states, setStates] = useState<State[]>([]);
+    const [cities, setCities] = useState<City[]>([]);
+
+    const [category, setCategory] = useState<string>();
+    const [title, setTitle] = useState<string>();
+    const [price, setPrice] = useState<any>('R$ ');
+    const [state, setState] = useState<string | undefined>('');
+    const [city, setCity] = useState<string | undefined>('');
+    const [disableCity, setDisableCity] = useState(false);
 
     const [categories, setCategories] = useState<Category[]>([]);
 
@@ -24,39 +52,112 @@ const Anunciar = () => {
         fetchData();
     }, [])
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCity(user?.personalData.address.city);
+        setState(user?.personalData.address.state);
+
+        setDisableCity(event.target.checked);
+    }
+
+    useEffect(() => {
+        fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+          .then(res => res.json())
+          .then(data => setStates(data));
+    }, []);
+    
+      useEffect(() => {
+        if (state) {
+          fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state}/municipios`)
+            .then(res => res.json())
+            .then(data => setCities(data));
+        } else {
+            setCities([]);
+        }
+    }, [state]);
+
+    const formatPrice = (price: string) => {
+        const numeric = price.replace(/\D/g, '');
+
+        if (!numeric) return 'R$ ';
+
+        const number = parseFloat(numeric) / 100;
+
+        return number.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+    }
+
     return (
-        <div>
-            <h1>Anunciar Produto</h1>
+        <div className={style.Container}>
+            <Header user={user}></Header>
 
-            <div>
-                <div className={style.SelectContainer}>
-                    <label htmlFor="category">Categoria</label>
-                    <select name="category" id="category">
-                    {categories.map((cat) => (
-                        <option value={cat._id}>
-                            <span>{cat.name}</span>
-                        </option>
-                    ))}
-                    </select>
+            <div className={style.Form}>
+                <h1>Anunciar Produto</h1>
+                <div className={style.FormGrid}>
+                    <div className={`${style.InputContainer} ${style.Category}`}>
+                        <label htmlFor="category">Categoria</label>
+                        <select name="category" id="category" value={category} className={style.Input} onChange={e => { setCategory(e.target.value) }}>
+                            {categories.map((cat) => (
+                                <option className={style.SelectOption} key={cat._id} value={cat._id}>
+                                    {cat.name}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className={`${style.InputContainer} ${style.Title}`}>
+                        <label htmlFor="title">Título</label>
+                        <input name="title" type="text" value={title} onChange={e => setTitle(e.target.value)} className={`${style.Input} ${style.InputTitle}`} />
+                    </div>
+                    <div className={`${style.InputContainer} ${style.Price}`}>
+                        <label htmlFor="price">Preço</label>
+                        <input name="price" type="string" value={price} onChange={e => setPrice(formatPrice(e.target.value))} className={style.Input} />
+                    </div>
+                    <div className={`${style.InputContainer} ${style.State}`}>
+                        <label htmlFor="state">Estado</label>
+                        <select name="state" id="state" className={style.Input} value={state} disabled={disableCity} onChange={e => { setState(e.target.value); setCity('') }}>
+                            {states.map(stt => (
+                                <option key={stt.id} value={stt.sigla}>{stt.nome}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className={`${style.InputContainer} ${style.City}`}>
+                        <label htmlFor="city">Cidade</label>
+                        <select name="city" id="city" className={style.Input} value={city} disabled={disableCity} onChange={e => { setCity(e.target.value) }}>
+                            {cities.map(ct => (
+                                <option key={ct.id} value={ct.nome}>{ct.nome}</option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className={style.CheckBox}>
+                        <label className={style.CheckBoxLabel} htmlFor="disablecity">Mesma localização da conta?</label>
+                        <input
+                            className={style.CheckInput}
+                            type="checkbox"
+                            name="disablecity"
+                            id="disablecity"
+                            checked={disableCity}
+                            onChange={handleChange}
+                        />
+                    </div>
+
                 </div>
-                <div><label htmlFor="brand">Marca</label><input type="text" /></div>
-                <div><label htmlFor="price">Preço</label><input type="text" /></div>
-                <div><label htmlFor="state">Estado</label><input type="select" /></div>
-                <div><label htmlFor="city">Cidade</label><input type="text" /></div>
-            </div>
 
-            <div>
-                <h1>Adicionar Fotos</h1>
                 <div>
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
-                    <img src="" alt="" />
+                    <h1>Adicionar Fotos</h1>
+                    <div>
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                        <img src="" alt="" />
+                    </div>
                 </div>
-            </div>
 
-            <button>Anunciar</button>
+                <button>Anunciar</button>
+
+            </div>
+            <Footer></Footer>
         </div>
     )
 }
